27e936e556eb3338629f5c43d6a943bce3a4a5b6
kv8gh <vyaskaran1409@gmail.com>
2024-08-21 23:45:14 -0700

    adding url

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //TODO 1 enter your key from geocode here
-export const GEOCODE_API_KEY="";
+export const GEOCODE_API_KEY="66c6261d0dbf8580430983eql7b13a1";
 
 //TODO 2 enter your function app url here
-export const FUNCTION_APP_URL="http://localhost:7071/api/getHospitalsList";
+export const FUNCTION_APP_URL="https://fa-cloud-user-9.azurewebsites.net/api/getHospitalsList?code=+";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
